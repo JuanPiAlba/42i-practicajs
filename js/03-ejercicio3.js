63433ffb66be64011d49d527df8c3321bbeeb3ed
Juan Pablo Alba Fernandez <juanpialba@gmail.com>
2023-03-06 17:41:00 -0300

    me emocione y me meande a hacer todo sin inicializar git ni hacer ningun comit ni ramas, perdon, ahora pongo todo en reglas

---
+++ js/03-ejercicio3.js
@@ -0,0 +1,5 @@
+/*Dificultad:  🟢
+3.- Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+*/
+
+document.write(3+5)
---
+++ js/06-ejercicio6.js
@@ -0,0 +1,9 @@
+/*6.- Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.*/
+
+let num1 = prompt ('Ingrese un Numero')
+let num2 = prompt ('Ingrese otro Numero mas')
+
+if( num1>num2){
+    document.write(num1)
+}else{
+    document.write(num2)}
