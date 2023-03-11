9daa50132e1fb10c7a1fc9968d22b40fafdd1483
Juan Pablo Alba Fernandez <juanpialba@gmail.com>
2023-03-11 18:30:14 -0300

    actualizamos ejercicio numero 9 para tenerlo como eran los requerimientos, con condicionales

--- js/09-ejercicio9.js
+++ js/09-ejercicio9.js
@@ -26,25 +26,36 @@ for (let i = 0; i < frase.length; i++) {
 // charAt(indice); ontiene un caracter
 // substring (desde,hasta); obtiene un caracter
 
-const frase = prompt("Ingrese una frase");
+let frase = prompt("Ingrese una frase de 4 caracteres");
 // hola mundo (4)
-console.log(frase.length)
+console.log(frase.toLowerCase())
+console.log(frase.toUpperCase())
 
-if(frase.charAt(0) === 'a' || frase.charAt(0) === 'e'|| frase.charAt(0) === 'i' || frase.charAt(0) === 'o'|| frase.charAt(0) === 'u'){
-	document.write(frase.charAt(0))
+frase = frase.toLowerCase();
+// fra1se = 'comision 42i'
+
+if(frase.length<5){
+	
+	if(frase.charAt(0) === 'a' || frase.charAt(0) === 'e'|| frase.charAt(0) === 'i' || frase.charAt(0) === 'o'|| frase.charAt(0) === 'u'){
+		document.write(frase.charAt(0))
+	}
+	
+	if(frase.charAt(1) === 'a' || frase.charAt(1) === 'e'|| frase.charAt(1) === 'i' || frase.charAt(1) === 'o'|| frase.charAt(1) === 'u'){
+		document.write(frase.charAt(1))
+	}
+	
+	if(frase.charAt(2) === 'a' || frase.charAt(2) === 'e'|| frase.charAt(2) === 'i' || frase.charAt(2) === 'o'|| frase.charAt(2) === 'u'){
+		document.write(frase.charAt(2))
+	}
+	
+	if(frase.charAt(3) === 'a' || frase.charAt(3) === 'e'|| frase.charAt(3) === 'i' || frase.charAt(3) === 'o'|| frase.charAt(3) === 'u'){
+		document.write(frase.charAt(3))
+	}
+
+}else{
+	alert('Ingresaste una palabra que exede a los carateres solicitados.')
 }
 
-if(frase.charAt(1) === 'a' || frase.charAt(1) === 'e'|| frase.charAt(1) === 'i' || frase.charAt(1) === 'o'|| frase.charAt(1) === 'u'){
-	document.write(frase.charAt(1))
-}
-
-if(frase.charAt(2) === 'a' || frase.charAt(2) === 'e'|| frase.charAt(2) === 'i' || frase.charAt(2) === 'o'|| frase.charAt(2) === 'u'){
-	document.write(frase.charAt(2))
-}
-
-if(frase.charAt(3) === 'a' || frase.charAt(3) === 'e'|| frase.charAt(3) === 'i' || frase.charAt(3) === 'o'|| frase.charAt(3) === 'u'){
-	document.write(frase.charAt(3))
-}
 
 
 
